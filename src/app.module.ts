602a4dfd83933d1a19a329856a3e61b6b9028c67
leninner <mazabandalenin180@gmail.com>
2025-03-20 15:57:18 -0500

    chore: fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppDataSource } from './config/typeorm.config'
+import { AuthModule } from './auth/auth.module'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(AppDataSource.options),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
