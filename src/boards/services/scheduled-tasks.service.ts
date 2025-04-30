f49a8cfca9a695bc263860d5803577041b7d2199
leninner <mazabandalenin180@gmail.com>
2025-04-30 11:47:01 -0500

    chore: update readme

--- src/boards/services/scheduled-tasks.service.ts
+++ src/boards/services/scheduled-tasks.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm'
 import { Card } from '../entities/card.entity'
 import { ColumnRulesService } from './column-rules.service'
 import { TriggerType } from '../dto/column-rule-types'
+import { MailService } from './mail.service'
 
 @Injectable()
 export class ScheduledTasksService {
@@ -14,6 +15,7 @@ export class ScheduledTasksService {
     @InjectRepository(Card)
     private cardsRepository: Repository<Card>,
     private columnRulesService: ColumnRulesService,
+    private readonly mailService: MailService,
   ) {}
 
   /**
@@ -58,5 +60,68 @@ export class ScheduledTasksService {
 
     const processingPromises = cards.map((card) => this.columnRulesService.processDueDateApproaching(card))
     await Promise.all(processingPromises)
+  }
+
+  @Cron('0 14 * * *')
+  async notifyUpcomingDueDateToAccountant() {
+    const today = new Date()
+    today.setHours(0, 0, 0, 0)
+
+    const endOfDay = new Date(today)
+    endOfDay.setHours(23, 59, 59, 999)
+
+    const cards = await this.cardsRepository
+      .createQueryBuilder('card')
+      .innerJoinAndSelect('card.column', 'column')
+      .leftJoinAndSelect('card.customer', 'customer')
+      .leftJoinAndSelect('customer.accountant', 'accountant')
+      .where('card.dueDate BETWEEN :startOfDay AND :endOfDay', {
+        startOfDay: today,
+        endOfDay: endOfDay,
+      })
+      .andWhere('customer.accountant IS NOT NULL')
+      .getMany()
+
+    // Agrupar tarjetas por contador
+    const cardsByAccountant = cards.reduce((acc, card) => {
+      const accountantId = card.customer?.accountant?.id
+      if (accountantId) {
+        if (!acc[accountantId]) {
+          acc[accountantId] = {
+            accountant: card.customer.accountant,
+            cards: [],
+          }
+        }
+        acc[accountantId].cards.push(card)
+      }
+      return acc
+    }, {})
+
+    this.logger.log(`Found ${cards.length} cards with upcoming due dates`)
+    if (cards.length === 0) {
+      this.logger.log('No cards with upcoming due dates found')
+      return
+    }
+
+    const promises = Object.values(cardsByAccountant).map(({ accountant, cards }) =>
+      this.mailService.sendTypedTemplateEmail(
+        accountant.email as string,
+        'Tarjetas con fecha de vencimiento próxima',
+        'custom-message',
+        {
+          customMessage: `
+            <p>Hola ${accountant.name},</p>
+            <p>Tenemos las siguientes tarjetas con fecha de vencimiento próxima:</p>
+            <ul>
+              ${cards.map((card: Card) => `<li>Tarjeta: ${card.name}</li>`).join('')}
+            </ul>
+            <p>Por favor, revise los detalles en su cuenta de Kontador.</p>
+            <p>Si tiene alguna pregunta, no dude en contactar a su contador asignado.</p>
+          `,
+        },
+      ),
+    )
+
+    await Promise.all(promises)
   }
 }
